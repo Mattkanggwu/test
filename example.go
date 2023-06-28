52bb8cc40a56fa45834a53f4a32d5c40cfa44a88
Mattkanggwu <135286016+Mattkanggwu@users.noreply.github.com>
2023-06-27 22:40:54 -0400

    123

--- example.go
+++ example.go
@@ -2,7 +2,48 @@ package main
 
 import "fmt"
 
-func main() {
+// Stack represents a stack data structure
+type Stack struct {
+	data []interface{}
+}
 
-	fmt.Println("heelo")
+// Push adds an element to the top of the stack
+func (s *Stack) Push(element interface{}) {
+	s.data = append(s.data, element)
+}
+
+// Pop removes and returns the element from the top of the stack
+func (s *Stack) Pop() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	lastIndex := len(s.data) - 1
+	element := s.data[lastIndex]
+	s.data = s.data[:lastIndex]
+	return element
+}
+
+// IsEmpty checks if the stack is empty
+func (s *Stack) IsEmpty() bool {
+	return len(s.data) == 0
+}
+
+// Size returns the number of elements in the stack
+func (s *Stack) Size() int {
+	return len(s.data)
+}
+
+func main() {
+	stack := Stack{}
+
+	stack.Push(10)
+	stack.Push(20)
+	stack.Push(30)
+
+	fmt.Println("Stack size:", stack.Size())
+
+	for !stack.IsEmpty() {
+		element := stack.Pop()
+		fmt.Println("Popped element:", element)
+	}
 }
